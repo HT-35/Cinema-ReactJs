3f2164ede702a260667bd0e18e68c226f41c257b
HT-35 <huyfa352002@gmail.com>
2024-05-11 14:33:06 +0700

    initial git

---
+++ .history/src/App_20240511141547.jsx
@@ -0,0 +1,6 @@
+export default function App() {
+  return;
+  <div className="">
+    <h1 className="">Hello !</h1>;
+  </div>;
+}
---
+++ .history/src/App_20240511143017.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="">
+      <h1 className="text-red-600 text-2xl">Hello !</h1>;
+    </div>
+  );
+}
